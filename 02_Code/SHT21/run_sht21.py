d438972acc717525657ee94e2859780497424f5a
Daniel Lee <lee@isi-solutions.org>
2015-10-09 18:22:23 +0200

    Implement prototypical scheduler

--- 02_Code/SHT21/run_sht21.py
+++ 02_Code/SHT21/run_sht21.py
@@ -10,15 +10,25 @@
 ## Note  :                                                                      ##
 ##==============================================================================##
 
-import sht21_class
+from sht21_class import TemperatureSensor, HumiditySensor
 import time
 
+if __name__ == "__main__":
+    """
+    Prototype sensor polling scheduler.
 
-# Create an instance 
-sht21 = sht21_class.SHT21()
+    To be implemented:
+    1. The value read from the sensor ``val`` needs to be saved
+    in a file. Also, the file per sensor needs to be determined.
 
-
-while 1:
-    sht21.getTemperature()
-    sht21.getHumidity()
-    time.sleep(1)
+    2. Logic for failed sensor readings
+    """
+    start = time.time()
+    sensor_list = [(TemperatureSensor(), 60, "/dev/temp-sensor"),
+                   (HumiditySensor(), 60, "/dev/hum-sensor")]
+    while True:
+        runtime = int(time.time() - start)
+        for sensor, interval, logfile in sensor_list:
+            if not runtime % interval:
+                val = sensor.get_value()
+        time.sleep(0.2)
