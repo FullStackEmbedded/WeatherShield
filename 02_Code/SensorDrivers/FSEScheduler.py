b8ff509e7f0d800367922e0a02dded7686959bb2
Daniel Lee <lee@isi-solutions.org>
2015-11-12 21:45:34 +0000

    Implement scheduler for real application
    Sensor polling is performed by separate threads dispatched in the main
    function. Polling intervals, etc. are still hardwired.

--- 02_Code/SensorDrivers/FSEScheduler.py
+++ 02_Code/SensorDrivers/FSEScheduler.py
@@ -9,54 +9,71 @@
 ## Note  :                                                    ##
 ##============================================================##
 
-##IMPORTS
 from __future__ import print_function
-from threading import Thread
+
+
+import csv
+import os
+import shutil
 import time
+from datetime import datetime
+from tempfile import NamedTemporaryFile as tmp
+from threading import Thread
+
+
 from sht21_class import TemperatureSensor, HumiditySensor
 from mpl3115a2_class import AirPressureSensor
 from ds3231_class import RTC
 
-## GLOBALS
-DEBUG = 1
-Realtime = RTC()
-print("Start time: ", Realtime.get_value())
 
-
+CLOCK = RTC()
 
 class Task(Thread):
-    def __init__(self,sensorObject,period,outputFile,variable=""):
-        Thread.__init__(self)
-        self._sensor = sensorObject
-        self._sleepTime  = period
-        self._logFile    = outputFile
-        self._variableName = variable
 
+    """Safely record observations from sensors."""
+
+    def __init__(self, sensor, period, output_file):
+        """Initialize thread, pass on arguments intended for superclass."""
+        Thread.__init__(self)
+        self.sensor = sensor
+        self.period = period
+        self.output_file = output_file
         self.daemon = True
         self.start()
 
     def run(self):
-        """ """
+        """Safely remove stale observations and append to target file."""
+        if not os.path.exists(self.output_file):
+            open(self.output_file, "w").close()
         while True:
-            val = self._sensor.get_value()
-            if DEBUG:
-                print(Realtime.get_value(), "==> ",self._variableName, " = ", val)
-            time.sleep(self._sleepTime)
+            with open(self.output_file) as old, tmp(delete=False) as new:
+                reader = csv.reader(old)
+                writer = csv.writer(new)
+                for line in reader:
+                    ts = datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S")
+                    now = datetime.now()
+                    diff = now - ts
+                    if diff.seconds < 60 * 60 * 2:
+                        writer.writerow(line)
+                try:
+                    current_time = CLOCK.get_value()
+                    observation = self.sensor.get_value()
+                    writer.writerow((current_time, observation))
+                except IOError: pass
+            os.chmod(new.name, 0644)
+            shutil.move(new.name, self.output_file)
+            time.sleep(self.period)
 
-    def writeLog(self):
-        """ 
-        #TODO: write Log file
-        """
-        if DEBUG:
-            print(self._logFile,"written")
 
+if __name__ == "__main__":
+    """Dispatch sensor tasks, then stay alive."""
+    print("Start time: ", CLOCK.get_value())
+    task_sets = ((TemperatureSensor, 1, "/dev/sensor-temp"),
+                 (HumiditySensor, 1, "/dev/sensor-humi"),
+                 (AirPressureSensor, 1, "/dev/sensor-pres"))
+    for sensor, interval, target, in task_sets:
+        Task(sensor(), interval, target)
+        time.sleep(1)
+    while True:
+        time.sleep(float('inf'))
 
-Task1 = Task(TemperatureSensor(), 3,"/dev/temperature-sensor","Temperatur[C]")
-time.sleep(1)
-Task2 = Task(HumiditySensor() , 5,"/dev/humidity-sensor","Humidity[%]")
-time.sleep(1)
-Task3 = Task(AirPressureSensor(), 4, "/dev/AirPressure-sensor","Pressure[Pa]")
-
-
-while True:
-    pass
